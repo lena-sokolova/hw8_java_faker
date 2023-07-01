c2918addd889f183f1616e0732d33b800140ed2a
Elena Sokolova <>
2023-07-02 01:28:12 +0200

    fixed format

--- src/test/java/com/demoqa/pages/RegistrationPage.java
+++ src/test/java/com/demoqa/pages/RegistrationPage.java
@@ -19,26 +19,26 @@ public class RegistrationPage {
     CalendarComponent calendarComponent = new CalendarComponent();
 
     SelenideElement
-        practiceFormWrapper = $(".practice-form-wrapper"),
-        firstNameInput = $("#firstName"),
-        lastNameInput = $("#lastName"),
-        userEmailInput = $("#userEmail"),
-        genderWrapper = $("#genterWrapper"),
-        userNumberInput = $("#userNumber"),
-        birthDayInput = $("#dateOfBirthInput"),
-        subjectInput = $("#subjectsInput"),
-        hobbiesWrapper = $("#hobbiesWrapper"),
-        uploadFileBtn = $("#uploadPicture"),
-        currentAddressInput = $("#currentAddress"),
-        state = $("#state"),
-        stateCityWrapper = $("#stateCity-wrapper"),
-        city = $("#city"),
-        submitBtn = $("#submit"),
-        modalDialog = $(".modal-dialog"),
-        modalDialogTitle = $("#example-modal-sizes-title-lg");
+            practiceFormWrapper = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            birthDayInput = $("#dateOfBirthInput"),
+            subjectInput = $("#subjectsInput"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            uploadFileBtn = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            state = $("#state"),
+            stateCityWrapper = $("#stateCity-wrapper"),
+            city = $("#city"),
+            submitBtn = $("#submit"),
+            modalDialog = $(".modal-dialog"),
+            modalDialogTitle = $("#example-modal-sizes-title-lg");
 
-    public RegistrationPage openPage () {
-        open ("/automation-practice-form");
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
         practiceFormWrapper.shouldHave(text(formTitle));
 
         return this;
@@ -50,76 +50,78 @@ public class RegistrationPage {
         return this;
     }
 
-    public RegistrationPage setFirstName (String value) {
+    public RegistrationPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setLastName (String value) {
+    public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setUserEmail (String value) {
+    public RegistrationPage setUserEmail(String value) {
         userEmailInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setGender (String value) {
-        genderWrapper.$(byText(value)).click();;
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+        ;
 
         return this;
     }
 
-    public RegistrationPage setUserNumber (String value) {
+    public RegistrationPage setUserNumber(String value) {
         userNumberInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setBirthDay (String day, String month, String year) {
+    public RegistrationPage setBirthDay(String day, String month, String year) {
         birthDayInput.click();
         calendarComponent.setDate(day, month, year);
 
         return this;
     }
 
-    public RegistrationPage setSubject (String value) {
+    public RegistrationPage setSubject(String value) {
         subjectInput.setValue(value).pressEnter();
 
         return this;
     }
 
-    public RegistrationPage setHobby (String value) {
-        hobbiesWrapper.$(byText(value)).click();;
+    public RegistrationPage setHobby(String value) {
+        hobbiesWrapper.$(byText(value)).click();
+        ;
 
         return this;
     }
 
-    public RegistrationPage uploadPicture (String value) {
+    public RegistrationPage uploadPicture(String value) {
         uploadFileBtn.uploadFromClasspath(value);
         ;
 
         return this;
     }
 
-    public RegistrationPage setCurrentAddress (String value) {
+    public RegistrationPage setCurrentAddress(String value) {
         currentAddressInput.setValue(value);
 
         return this;
     }
 
-    public RegistrationPage setState (String value) {
+    public RegistrationPage setState(String value) {
         state.click();
         stateCityWrapper.$(byText(value)).click();
 
         return this;
     }
 
-    public RegistrationPage setCity (String value) {
+    public RegistrationPage setCity(String value) {
         city.click();
         city.$(byText(value)).click();
 
